152291e68bbd9e2b47045be46ca5c5f5df23aead
Mjte254 <ombogimaxi96@gmail.com>
2024-08-24 01:02:12 +0300

    AirBnB_clone

--- models/place.py
+++ models/place.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Defines the Place class."""
+"""
+Module for the Place class.
+"""
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
     """Represent a place.
+
     Attributes:
         city_id (str): The City id.
         user_id (str): The User id.
--- models/review.py
+++ models/review.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+"""
+Module for the Review class.
+"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
     """Represent a review.
+
     Attributes:
         place_id (str): The Place id.
         user_id (str): The User id.
